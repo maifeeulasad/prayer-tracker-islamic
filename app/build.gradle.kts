af50d212f62d1fadad03ff69a0abf4c5c2c91299
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-04-27 13:43:03 +0600

    [release]: set code to `5`; name to `1.4`;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.mua.prayertracker"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
